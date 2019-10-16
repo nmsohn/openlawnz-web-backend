6d1bb6fcf76bfdf07774066d635f8974c7f5627f
noel <noelsoong@live.com>
2019-10-16 22:39:07 +1300

    Cases routes

--- app/routes/cases.js
+++ app/routes/cases.js
@@ -1,7 +1,10 @@
 const Cases = require('../controller/cases')
 
 module.exports = router => {
-  router.post('/', Cases.create)
+  router.post('/', (req, res) => {
+    res.json(req.body)
+  })
+  router.post('/cases', Cases.create)
   // router.put('/:id', Cases.update)
   // router.del('/:id', Cases.delete)
 
