6a6ac9a91ef923af84fab4845361b0ec3a3435ec
noel <noelsoong@live.com>
2019-10-17 17:28:02 +1300

    Temp Manual Solution to authorization in controller

--- app/controller/cases.js
+++ app/controller/cases.js
@@ -3,6 +3,12 @@ const Cases = require('../model/cases')
 export function create (req, res) {
   const data = req.body
   const userId = req.user.sub || 0
+
+  // Auth Check, to be replaced by authorization middlewares
+  if (req.user['https://openlaw.nz/roles'] !== 'admin') {
+    res.status(403).send('Forbidden')
+  }
+
   Cases.insert(data, userId, err => {
     if (err) {
       res.status(500).send(err.stack)
---
+++ middlewares/auth/scope.js
@@ -0,0 +1,28 @@
+// Use this as a unified location to look for scope of each role
+
+const scope =
+  {
+    admin: {
+      select: [],
+      post: [],
+      get: [],
+      put: [],
+      delete: []
+    },
+    developer: {
+      select: [],
+      post: [],
+      get: [],
+      put: [],
+      delete: []
+    },
+    editor: {
+      select: [],
+      post: [],
+      get: [],
+      put: [],
+      delete: []
+    }
+  }
+
+module.exports = scope
